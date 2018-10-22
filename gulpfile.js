2cd9b3a8b9d3d51014057ea1c8b118bd57d50837
Giang Peter <giangmd93@gmail.com>
2018-10-22 20:43:03 +0700

    the first commit

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    htmlPartial = require('gulp-html-partial'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css'),
+    clean = require('gulp-rimraf'),
+    runSequence = require('run-sequence');
+
+gulp.task('clean', [], function() {
+    console.log("Clean all files in build folder");
+
+    return gulp.src("dist/*", { read: false }).pipe(clean());
+});
+
+gulp.task('sass', function() {
+    console.log("Sass task run");
+
+    gulp.src('src/scss/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(minifyCSS())
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('css', function() {
+    console.log("Css task run");
+
+    gulp.src([
+            'node_modules/bootstrap/dist/css/bootstrap.css',
+            'node_modules/@fortawesome/fontawesome-free/css/all.css',
+            'src/css/**/*.css'
+        ])
+        .pipe(minifyCSS())
+        .pipe(concat('lib.min.css'))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('js', function() {
+    console.log("JS task run");
+
+    gulp.src([
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/bootstrap/dist/js/bootstrap.js',
+            'src/js/**/*.js'
+        ])
+        .pipe(uglify())
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('images', function() {
+    console.log("Images task run");
+
+    gulp.src([
+            'src/images/**/*'
+        ])
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('html', function () {
+    console.log("HTML task run");
+
+    gulp.src(['src/*.html'])
+        .pipe(htmlPartial({
+            basePath: 'src/partials/'
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function() {
+    runSequence('clean', 'sass', 'css', 'js', 'images', 'html', function() {
+        console.log('Run task done');
+        // done();
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.run('default');
+
+    gulp.watch('src/scss/**/*.scss', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('css');
+    });
+
+    gulp.watch('src/css/**/*.css', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('css');
+    });
+
+    gulp.watch('src/js/**/*.js', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('js');
+    });
+
+    gulp.watch('src/images/**/*', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('images');
+    });
+
+    gulp.watch('src/**/*.html', function (event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('html');
+    });
+});
+
+
---
+++ readme.md
@@ -0,0 +1,83 @@
+# Front end structure with gulp
+
+A basic front end structure using gulp. Help you save many time in the first time working with gulp
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+node && npm 
+```
+
+### Installing
+
+Clone this project from git
+
+```
+git clone https://github.com/giangmd/frontend-gulp.git your-project
+```
+
+Run install package
+
+```
+npm install
+```
+
+Run build gulp for development
+```
+npm run dev
+```
+or
+```
+gulp watch
+```
+
+Or Run build gulp for build
+```
+npm run build
+```
+or 
+```
+gulp
+```
+
+## Built With
+
+* [Webpack](https://webpack.js.org/)
+* [Bootstrap](https://getbootstrap.com/)
+* [Font Awesome](https://fontawesome.com/)
+
+## Contributing
+
+If you have time and wanna to contribute with me. Please, create a pull request.
+
+## Versioning
+
+1.0.0
+
+1.1.0
+- Add **html-loader** and **html-webpack-plugin**
+- Change struct html file to *src/index.html* and extends from *src/components* templates
+- Check after run build in *dist/index.html*
+
+## Authors
+
+* **Mai Dong Giang** - *Owner* - [IZWAY Co., Ltd](https://www.izway.vn/)
+
+See also the list of [contributors](https://github.com/giangmd/frontend-webpack/graphs/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used
+* Inspiration
+* etc
+
